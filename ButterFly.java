711d9f0da65a8ee4b96e14c2b017f247ebff50a5
MaheshKhadayat <khadayatmahesh198@gmail.com>
2024-12-24 13:32:01 +0530

    Add files via upload
    This code gives us  the pattern of  butterfluy.
    
    Signed-off-by: MaheshKhadayat <khadayatmahesh198@gmail.com>

---
+++ ButterFly.java
@@ -0,0 +1,34 @@
+public class ButterFly {
+
+    public static void main(String[] args) {
+        int N=5;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=0;j<2*(N-i)-2;j++){
+                System.out.print("  ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N-i;j++){
+                System.out.print("* ");
+            }
+            for(int j=0;j<2*i;j++){
+                System.out.print("  ");
+            }
+            for(int j=0;j<N-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
